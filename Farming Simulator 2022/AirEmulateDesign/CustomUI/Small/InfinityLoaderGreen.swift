56bebc7b3689934c9b4cbbef7778b2ffb5aa31a3
flash1729 <sherlockedaditya@gmail.com>
2025-01-08 17:24:55 +0530

    loader edited even more

--- Farming Simulator 2022/AirEmulateDesign/CustomUI/Small/InfinityLoaderGreen.swift
+++ Farming Simulator 2022/AirEmulateDesign/CustomUI/Small/InfinityLoaderGreen.swift
@@ -7,45 +7,6 @@
 
 import SwiftUI
 
-//struct InfinityLoaderGreen: View {
-//    @State var progressLoader: Int = 0
-//    let bigSize = UIDevice.current.userInterfaceIdiom == .pad
-//    @State private var stopTimer: Bool = false
-//    var body: some View {
-//        Circle()
-//            .fill(Color.clear)
-//            .overlay(
-//                AngularGradient(colors: [ColorTurboGear.colorPicker(.green).opacity(0), ColorTurboGear.colorPicker(.green).opacity(0.5), ColorTurboGear.colorPicker(.green)], center: .center)
-//                    .rotationEffect(.degrees(Double(progressLoader * 45)))
-//            )
-//            .clipShape(Circle())
-//            .mask {
-//                Image(IconTurboGear.LoaderMaskForImage)
-//                    .resizable()
-//                    .scaledToFit()
-//            }
-//            .rotationEffect(.degrees(270))
-//            .onAppear(){
-//                infinityLoading()
-//            }
-//    }
-//
-//    private func infinityLoading() {
-//        Timer.scheduledTimer(withTimeInterval: 0.075, repeats: true) { time in
-//            if stopTimer {
-//                self.progressLoader = 0
-//                time.invalidate()
-//            } else {
-//                if self.progressLoader < 128 {
-//                    self.progressLoader += 1
-//                } else {
-//                    self.progressLoader = 0
-//                }
-//            }
-//        }
-//    }
-//}
-
 struct InfinityLoaderGreen: View {
     @State private var rotation = 0.0
     let bigSize = UIDevice.current.userInterfaceIdiom == .pad
@@ -62,7 +23,6 @@ struct InfinityLoaderGreen: View {
 //                    .foregroundColor(ColorTurboGear.colorPicker(.green))
                     .rotationEffect(.degrees(Double(progressLoader * 45)))
             )
-            .clipShape(Circle())
             .frame(width: bigSize ? 48 : 32, height: bigSize ? 48 : 32)
             .onAppear(){
                 infinityLoading()
